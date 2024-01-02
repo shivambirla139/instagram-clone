d32159ac0a62615dcaf6a8f4b140d4458b1ba50e
shivambirla139 <shivambirla139@gmail.com>
2024-01-02 13:45:37 +0530

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+//'mongodb://localhost:27017/test'
+//'mongodb+srv://shivambirla139:password108@cluster0.qvaixgn.mongodb.net/'
+exports.connectDatabase = async () => {
+  try {
+    const con = await mongoose.connect('mongodb://localhost:27017/socialmedia',{
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          })
+    console.log(`Database Connected: ${con.connection.host}`);
+  }catch (e){
+    console.log(e);
+  }
+};
